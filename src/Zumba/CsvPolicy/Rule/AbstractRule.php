1a437e711d6092f33b06acfad48b25f7d44124e5
Stephen Young <young.steveo@gmail.com>
2013-12-19 15:53:02 -0500

    Convert spaces to tabs

--- src/Zumba/CsvPolicy/Rule/AbstractRule.php
+++ src/Zumba/CsvPolicy/Rule/AbstractRule.php
@@ -14,53 +14,53 @@ abstract class AbstractRule {
 	 * @access protected
 	 * @var array
 	 */
-    protected $tokens = [];
+	protected $tokens = [];
 
-    /**
-     * Returns the inputs that have been validated against
-     *
-     * @access public
-     * @param void
-     * @return array
-     */
-    public function getTokens() {
-        return $this->tokens;
-    }
+	/**
+	 * Returns the inputs that have been validated against
+	 *
+	 * @access public
+	 * @param void
+	 * @return array
+	 */
+	public function getTokens() {
+		return $this->tokens;
+	}
 
-    /**
-     * Checks if the input has been parsed before
-     *
-     * @access protected
-     * @param mixed $input
-     * @return boolean
-     */
+	/**
+	 * Checks if the input has been parsed before
+	 *
+	 * @access protected
+	 * @param mixed $input
+	 * @return boolean
+	 */
 	protected function isUnique($input){
-        $sumOccurrences = function($v, $n){
-            return $v + (int)($n === $this->scalar);
-        };
+		$sumOccurrences = function($v, $n){
+			return $v + (int)($n === $this->scalar);
+		};
 		return array_reduce($this->tokens, $sumOccurrences->bindTo((object)$input), 0) === 1;
 	}
 
-    /**
-     * Store the input values
-     *
-     * @access public
-     * @param mixed $input
-     * @return void
-     */
-    public function validate($input) {
-        $this->tokens[] = $input;
-    }
+	/**
+	 * Store the input values
+	 *
+	 * @access public
+	 * @param mixed $input
+	 * @return void
+	 */
+	public function validate($input) {
+		$this->tokens[] = $input;
+	}
 
-    /**
-     * Get the error message regarding this rule
-     *
-     * Is passed the currently offending value
-     *
-     * @abstract
-     * @access public
-     * @param $input string
-     * @return string
-     */
-    abstract public function getErrorMessage($input);
+	/**
+	 * Get the error message regarding this rule
+	 *
+	 * Is passed the currently offending value
+	 *
+	 * @abstract
+	 * @access public
+	 * @param $input string
+	 * @return string
+	 */
+	abstract public function getErrorMessage($input);
 }
