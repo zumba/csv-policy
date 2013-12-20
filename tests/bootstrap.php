3456eaf37a8d640381920332078aa567db62579e
Stephen Young <young.steveo@gmail.com>
2013-12-20 14:01:55 -0500

    Test suite is now PSR-1 compliant

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,4 @@
 <?php
 
-if (!defined('RULES_PATH')){
-	define('RULES_PATH', __DIR__);
-}
-
-if (!defined('FIXTURE_PATH')){
-	define('FIXTURE_PATH', RULES_PATH . '/Zumba/CsvPolicy/Test/Fixture');
-}
-
+require 'config.php';
 require dirname(__DIR__) . '/vendor/autoload.php';
